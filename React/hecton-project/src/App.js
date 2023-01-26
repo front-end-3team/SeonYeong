5b4e5c8dd0431e4bca6796e7df5c7e55c18583b0
seonyeong719 <sy904280@gmail.com>
2023-01-26 23:54:24 +0900

    Add: react 컴포넌트 분할연습
    아직 연습 중이라 미완성입니다

---
+++ React/hecton-project/src/App.js
@@ -0,0 +1,15 @@
+import DoctorsFooter from "./components/DoctorsFooter";
+import DoctorsHeader from "./components/DoctorsHeader";
+import DoctorsMain from "./components/DoctorsMain";
+
+function App() {
+    return (
+        <div>
+            <DoctorsHeader></DoctorsHeader>
+            <DoctorsMain></DoctorsMain>
+            <DoctorsFooter></DoctorsFooter>
+        </div>
+    );
+}
+
+export default App;
