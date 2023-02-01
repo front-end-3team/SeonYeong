c3a0d0b7157facdffc9d408d0be86ef326190891
seonyeong719 <sy904280@gmail.com>
2023-02-02 02:27:49 +0900

    Docs: hook 함수 예습
    hook 함수 에습 연습 추가 업로드

--- React/hook/src/pages/UseEffect.js
+++ React/hook/src/pages/UseEffect.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 
 function UseEffect() {
-    const [name, setName] = useState();
-    const [count, setCount] = useState();
+    const [name, setName] = useState("");
+    const [count, setCount] = useState(0);
+
+    const randSetName = (e) => setName(e.target.value);
+
+    useEffect(() => {
+        console.log("랜더링");
+        console.log({ name, count });
+    }, [name, count]);
 
     return (
         <>
             <p>text 입력</p>
-            <input type="text" placeholder="text" />
-            <button onClick={() => setCount(count + 1)}></button>
+            <input type="text" value={name} onChange={randSetName} />
+            <p>카운트 : {count}</p>
+            <button onClick={() => setCount(count + 1)}>+1 클릭</button>
+            <button onClick={() => setCount(count - 1)}>-1 클릭</button>
         </>
     );
 }
