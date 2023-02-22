46d099de8b9ab84ed5954d45705cd2f8a32fca10
seonyeong719 <sy904280@gmail.com>
2023-02-23 04:24:37 +0900

    Add: state1~2 번 문제 업로드
    state1~2 번 문제 풀이 완료

--- hookstest/Hooks/src/hooks/2.state/state1.js
+++ hookstest/Hooks/src/hooks/2.state/state1.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import PlayListMock from "../../__mock__/playList.json";
 
 function State1() {
-  /* 
+    /* 
     문제 1.
 
     state를 다루기 위한 기초 문제입니다.
@@ -11,32 +12,67 @@ function State1() {
     삭제 버턴을 눌렀을 때 데이터가 삭제될 수 있도록 해주세요
   */
 
-  console.log(PlayListMock.playlist);
-  /* 데이터 콘솔에 찍어두었으니 확인해볼 것 */
+    // const [onPlusSong, setOnPlusSong] = useState(false);
 
-  return (
-    <>
-      <h1>문제1</h1>
-      <ul>
-        {/* list */}
-        {/* 예시 데이터입니다 */}
-        <li>
-          <h3>Summer</h3>
-          <p>Joe Hisaishi</p>
-        </li>
-      </ul>
-      <div>
-        <p>
-          곡명 : <input />
-        </p>
-        <p>
-          가수/작곡 : <input />
-        </p>
-        <p>
-          <button>추가</button>
-        </p>
-      </div>
-    </>
-  );
+    const [musicList, setMusicList] = useState(PlayListMock.playlist);
+    const [addList, setAddList] = useState({
+        title: "",
+        singer: "",
+    });
+
+    const { title, singer } = addList;
+    // 인풋 값
+    const onAddSong = (e) => {
+        const { name, value } = e.target;
+        setAddList({
+            ...addList,
+            [name]: value,
+        });
+    };
+    // 추가버튼
+    const onAddSongList = () => {
+        const list = {
+            title,
+            signer: singer,
+        };
+
+        setMusicList([...musicList, list]);
+
+        setAddList({
+            title: "",
+            singer: "",
+        });
+    };
+    console.log(musicList);
+    // 삭제버튼
+    const removeBtn = (title) => {
+        setMusicList(musicList.filter((el) => el.title !== title));
+    };
+
+    return (
+        <>
+            <ul>
+                {musicList.map((e) => (
+                    <li>
+                        <h3>{e.title}</h3>
+                        <p>{e.signer}</p>
+                        <button onClick={() => removeBtn(e.title)}>삭제</button>
+                        <hr />
+                    </li>
+                ))}
+            </ul>
+            <div>
+                <p>
+                    곡명 : <input name="title" value={title} onChange={onAddSong} />
+                </p>
+                <p>
+                    가수/작곡 : <input name="singer" value={singer} onChange={onAddSong} />
+                </p>
+                <p>
+                    <button onClick={onAddSongList}>추가</button>
+                </p>
+            </div>
+        </>
+    );
 }
 export default State1;
--- hookstest/Hooks/src/hooks/2.state/state2.js
+++ hookstest/Hooks/src/hooks/2.state/state2.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Comment from "../../components/2.state/comment";
 
 function State2() {
-  /*  
+    /*  
     문제 2.
 
     Q1. 아래 작성된 state의 mock data를 활용하여
@@ -16,136 +16,176 @@ function State2() {
             3. 댓글 삭제 기능 ( 본인이 작성한 댓글만 삭제할 수 있습니다, myComment 활용 )
     */
 
-  const [post, setPost] = useState({
-    title: "안녕하세요 여러분 김성용 강사입니다 :)",
-    content: "오늘도 모두 화이팅입니다!",
-    User: {
-      nickname: "김성용",
-      age: 20,
-      height: 190,
-    },
-    Comments: [
-      {
+    const [post, setPost] = useState({
+        title: "안녕하세요 여러분 김성용 강사입니다 :)",
+        content: "오늘도 모두 화이팅입니다!",
         User: {
-          nickname: "김사과",
+            nickname: "김성용",
+            age: 20,
+            height: 190,
         },
-        content: "오늘도 화이팅입니다!",
-        myComment: false,
-      },
-      {
-        User: {
-          nickname: "반하나",
-        },
-        content: "오늘도 화이팅입니다!",
-        myComment: false,
-      },
-      {
-        User: {
-          nickname: "오렌지",
-        },
-        content: "오늘도 화이팅입니다!",
-        myComment: false,
-      },
-      {
-        User: {
-          nickname: "이멜론",
-        },
-        content: "오늘도 화이팅입니다!",
-        myComment: false,
-      },
-      {
-        User: {
-          nickname: "박수박",
-        },
-        content: "오늘도 화이팅입니다!",
-        myComment: false,
-      },
-    ],
-  });
+        Comments: [
+            {
+                id: 1,
+                User: {
+                    nickname: "김사과",
+                },
+                content: "오늘도 화이팅입니다!",
+                myComment: false,
+            },
+            {
+                id: 2,
+                User: {
+                    nickname: "반하나",
+                },
+                content: "오늘도 화이팅입니다!",
+                myComment: false,
+            },
+            {
+                id: 3,
+                User: {
+                    nickname: "오렌지",
+                },
+                content: "오늘도 화이팅입니다!",
+                myComment: false,
+            },
+            {
+                id: 4,
+                User: {
+                    nickname: "이멜론",
+                },
+                content: "오늘도 화이팅입니다!",
+                myComment: false,
+            },
+            {
+                id: 5,
+                User: {
+                    nickname: "박수박",
+                },
+                content: "오늘도 화이팅입니다!",
+                myComment: false,
+            },
+        ],
+    });
 
-  return (
-    <S.Wrapper>
-      <h1>문제2</h1>
-      <S.PostBox>
-        <S.PostTitle>제목: {post.title}</S.PostTitle>
-        <S.PostContent>내용: {post.content}</S.PostContent>
-      </S.PostBox>
-      <S.PostInfo>
-        <p>
-          작성자: <span>{post.User.nickname}</span>
-        </p>
-        <p>
-          작성자 나이: <span>{post.User.age}</span>
-        </p>
-        <p>
-          작성자 키: <span>{post.User.height}</span>
-        </p>
-      </S.PostInfo>
-      <div>
-        <p>
-          댓글 수: <span>{post.Comments.length}</span>
-        </p>
-        <input placeholder="작성자" />
-        <input placeholder="댓글 내용" />
-        <button>댓글 작성</button>
-      </div>
-      <S.CommentList>
-        {/* list */}
-        {/* 예시 데이터 */}
-        <Comment />
-      </S.CommentList>
-    </S.Wrapper>
-  );
+    const [inputUser, setInputUser] = useState("");
+    const [inputContent, setInputContent] = useState("");
+
+    const onAddUser = (e) => {
+        setInputUser(e.target.value);
+    };
+
+    const onAddContent = (e) => {
+        setInputContent(e.target.value);
+    };
+
+    // 댓글 추가 기능
+    const onList = () => {
+        const newPost = { ...post };
+        const newAddPost = {
+            id: post.Comments.length + 1,
+            User: {
+                nickname: inputUser,
+            },
+            content: inputContent,
+            myComment: true,
+        };
+        newPost.Comments.push(newAddPost);
+        setPost(newPost);
+    };
+
+    // 삭제 버튼
+    const onRemoveBtn = (e) => {
+        if (window.confirm("정말 삭제 하시겠습니까?"))
+            setPost((prev) => ({
+                ...prev,
+                Comments: [...prev.Comments.filter((el) => el.id !== e.id)],
+            }));
+    };
+
+    return (
+        <S.Wrapper>
+            <h1>문제2</h1>
+            <S.PostBox>
+                <S.PostTitle>제목: {post.title}</S.PostTitle>
+                <S.PostContent>내용: {post.content}</S.PostContent>
+            </S.PostBox>
+            <S.PostInfo>
+                <p>
+                    작성자: <span>{post.User.nickname}</span>
+                </p>
+                <p>
+                    작성자 나이: <span>{post.User.age}</span>
+                </p>
+                <p>
+                    작성자 키: <span>{post.User.height}</span>
+                </p>
+            </S.PostInfo>
+            <div>
+                <p>
+                    댓글 수: <span>{post.Comments.length}</span>
+                </p>
+                <input onChange={onAddUser} name="name" placeholder="작성자" />
+                <input onChange={onAddContent} name="content" placeholder="댓글 내용" />
+                <button onClick={onList}>댓글 작성</button>
+            </div>
+            <S.CommentList>
+                {post.Comments.map((list) => (
+                    <Comment post={post} list={list} onRemoveBtn={onRemoveBtn} />
+                ))}
+            </S.CommentList>
+        </S.Wrapper>
+    );
 }
 export default State2;
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 
 const PostBox = styled.div`
-  background-color: #999;
-  width: 360px;
-  padding: 10px;
+    background-color: #999;
+    width: 360px;
+    padding: 10px;
 `;
 
 const PostTitle = styled.p`
-  font-size: 20px;
-  font-weight: bold;
+    font-size: 20px;
+    font-weight: bold;
 `;
 
 const PostContent = styled.p`
-  color: #fff;
+    color: #fff;
 `;
 
 const PostInfo = styled.div`
-  width: 360px;
-  border: 3px solid #f00;
-  padding: 10px;
-  margin: 10px;
+    width: 360px;
+    border: 3px solid #f00;
+    padding: 10px;
+    margin: 10px;
 
-  p {
-    display: flex;
-    justify-content: space-around;
-  }
+    p {
+        display: flex;
+        justify-content: space-around;
+    }
 
-  span {
-    font-weight: bold;
-  }
+    span {
+        font-weight: bold;
+    }
 `;
 
 const CommentList = styled.ul`
-  width: 960px;
+    width: 960px;
 `;
 
 const S = {
-  Wrapper,
-  PostBox,
-  PostTitle,
-  PostContent,
-  PostInfo,
-  CommentList,
+    Wrapper,
+    PostBox,
+    PostTitle,
+    PostContent,
+    PostInfo,
+    CommentList,
 };
