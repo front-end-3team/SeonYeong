25aaf8727843c2bec3a18e3ad0a29fe3f8f94495
seonyeong719 <sy904280@gmail.com>
2023-03-05 18:36:30 +0900

    Feat: 2-1 context 문제 풀이 수정 완료

--- GlobalTest/src/App.js
+++ GlobalTest/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./adapters/router";
+import ContextModalPage from "./store/2_context";
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+    return (
+        <>
+            <ContextModalPage>
+                <RouterProvider router={router} />
+            </ContextModalPage>
+        </>
+    );
 }
 
 export default App;
--- GlobalTest/src/store/2_context.js
+++ GlobalTest/src/store/2_context.js
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import { createContext } from "react";
 export const myContext = createContext();
-// export const myContext2 = createContext();
+export const myContext2 = createContext();
 
-// const ContextModalPage = ({ children }) => {
-//     const [isModalOpen, setIsModalOpen] = useState(false);
+const ContextModalPage = ({ children }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-//     return (
-//         <>
-//             <myContext.Provider value={{ isModalOpen, setIsModalOpen }}>
-//                 {children}
-//             </myContext.Provider>
-//         </>
-//     );
-// };
-// export default ContextModalPage;
+    return (
+        <>
+            <myContext.Provider value={{ isModalOpen, setIsModalOpen }}>
+                {children}
+            </myContext.Provider>
+        </>
+    );
+};
+export default ContextModalPage;
